f95a21ab29905b0e7884f947bfe815f115e02f48
jinnieusLab <kitty14904@naver.com>
2025-11-14 21:33:55 +0900

    docs: Swagger Bin, Liquid API 설명 추가

--- src/main/java/com/smu/iot/domain/liquid/controller/LiquidController.java
+++ src/main/java/com/smu/iot/domain/liquid/controller/LiquidController.java
@@ -9,6 +9,7 @@ import com.smu.iot.domain.liquid.service.LiquidService;
 import com.smu.iot.domain.liquid.entitiy.PeriodType;
 import com.smu.iot.domain.liquid.entitiy.TrendMode;
 import com.smu.iot.global.apipayload.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -23,65 +24,92 @@ import java.time.LocalDate;
 public class LiquidController {
     private final LiquidService liquidService;
 
-    // 물통 생성
-    // 쓰레기통 생성 시 반드시 물통 생성
     @PostMapping("/by-bin/{binId}")
+    @Operation(
+            summary = "물통 생성",
+            description = "binId를 받아 물통을 생성"
+    )
     public ApiResponse<LiquidResponseDTO.CreateLiquidResultDTO> createLiquid(@PathVariable Long binId, @RequestBody LiquidRequestDTO.CreateLiquidDTO createLiquidDTO) {
         Liquid liquid = liquidService.createLiquid(binId, createLiquidDTO);
         return ApiResponse.onSuccess(LiquidSuccessCode.LIQUID_CREATED, LiquidConverter.toCreateLiquidResultDTO(liquid));
     }
 
-    // 특정 물통 정보 조회 (by binId)
     @GetMapping("/by-bin/{binId}")
+    @Operation(
+            summary = "특정 물통 조회 (by binId)",
+            description = "binId로 특정 물통을 조회"
+    )
     public ApiResponse<LiquidResponseDTO.LiquidPreviewDTO> readLiquidByBinId(@PathVariable Long binId) {
         Liquid liquid = liquidService.readLiquidByBinId(binId);
         return ApiResponse.onSuccess(LiquidSuccessCode.LIQUID_READ, LiquidConverter.toLiquidPreviewDTO(liquid));
     }
 
-    // 특정 물통 정보 조회 (by liquidId)
     @GetMapping("/{liquidId}")
+    @Operation(
+            summary = "특정 물통 조회 (by liquidId)",
+            description = "liquidId로 특정 물통을 조회"
+    )
     public ApiResponse<LiquidResponseDTO.LiquidPreviewDTO> readLiquidById(@PathVariable Long liquidId) {
         Liquid liquid = liquidService.readLiquidById(liquidId);
         return ApiResponse.onSuccess(LiquidSuccessCode.LIQUID_READ, LiquidConverter.toLiquidPreviewDTO(liquid));
     }
 
-    // 전체 liquid 정보 + 전체 무게 평균 조회
     @GetMapping
+    @Operation(
+            summary = "전체 물통 조회",
+            description = "전체 liquid 정보와 전체 무게 평균 조회"
+    )
     public ApiResponse<LiquidResponseDTO.LiquidPreviewListWithAverageDTO> readLiquids() {
         LiquidResponseDTO.LiquidPreviewListWithAverageDTO liquidsWithAverage = liquidService.readLiquids();
         return ApiResponse.onSuccess(LiquidSuccessCode.LIQUID_READ, liquidsWithAverage);
     }
 
-    // 특정 물통 무게 수정 (by binId)
     @PatchMapping("/by-bin/{binId}")
+    @Operation(
+            summary = "특정 물통 무게 수정(by binId)",
+            description = "binId와 무게를 받아 특정 물통 무게를 수정"
+    )
     public ApiResponse<LiquidResponseDTO.LiquidPreviewDTO> updateLiquidByBinId(@PathVariable Long binId, @RequestBody LiquidRequestDTO.UpdateLiquidDTO updateLiquidDTO) {
         Liquid liquid = liquidService.updateLiquidByBinId(binId, updateLiquidDTO);
         return ApiResponse.onSuccess(LiquidSuccessCode.LIQUID_READ, LiquidConverter.toLiquidPreviewDTO(liquid));
     }
 
-    // 특정 물통 무게 수정 (by liquidId)
     @PatchMapping("/{liquidId}")
+    @Operation(
+            summary = "특정 물통 무게 수정(by liquidId)",
+            description = "liquidId와 무게를 받아 특정 물통 무게를 수정"
+    )
     public ApiResponse<LiquidResponseDTO.LiquidPreviewDTO> updateLiquidById(@PathVariable Long liquidId, @RequestBody LiquidRequestDTO.UpdateLiquidDTO updateLiquidDTO) {
         Liquid liquid = liquidService.updateLiquidById(liquidId, updateLiquidDTO);
         return ApiResponse.onSuccess(LiquidSuccessCode.LIQUID_READ, LiquidConverter.toLiquidPreviewDTO(liquid));
     }
 
-    // 특정 물통 무게 4kg 이상(80% 이상) 찼는지 여부 조회
     @GetMapping("/overload/{liquidId}")
+    @Operation(
+            summary = "특정 물통 과적 조회",
+            description = "liquidId로 특정 물통의 무게가 4kg 이상 찼는지 여부를 조회"
+    )
     public ApiResponse<Boolean> isLiquidOverloadedById(@PathVariable Long liquidId) {
         Boolean overloaded = liquidService.isLiquidOverloadedById(liquidId);
         return ApiResponse.onSuccess(LiquidSuccessCode.LIQUID_READ, overloaded);
     }
 
-    // 물통 무게 4kg 이상(80% 이상) 찬 물통 전체 조회
     @GetMapping("/overload")
+    @Operation(
+            summary = "전채 물통 과적 조회",
+            description = "4kg 이상 찬 물통의 리스트를 조회"
+    )
     public ApiResponse<LiquidResponseDTO.LiquidPreviewListDTO> readLiquidsOverloadedById() {
         LiquidResponseDTO.LiquidPreviewListDTO liquidPreviewList = liquidService.readLiquidsOverloaded();
         return ApiResponse.onSuccess(LiquidSuccessCode.LIQUID_READ, liquidPreviewList);
     }
 
-    // 특정 물통 트렌드(시간 구간대별 무게/누적합) 조회 (by binId) (월별, 주별, 일별)
+
     @GetMapping("/by-bin/{binId}/trend")
+    @Operation(
+            summary = "특정 물통 트렌드 조회 (by binId)",
+            description = "binId로 특정 물통 트렌드(시간 구간대별 무게/누적합) 조회 (월별, 주별, 일별)"
+    )
     public ApiResponse<Object> readLiquidTrendByBinID(@PathVariable Long binId, @RequestParam(name = "period", defaultValue = "DAILY") PeriodType period,
                                                                                  @RequestParam(name = "date", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
                                                                                  @RequestParam(name = "mode", defaultValue = "TREND") TrendMode mode) {
@@ -89,8 +117,11 @@ public class LiquidController {
         return ApiResponse.onSuccess(LiquidSuccessCode.LIQUID_READ, liquidTrend);
     }
 
-    // 특정 물통 트렌드(시간 구간대별 무게/누적합) 조회 (by liquidId) (월별, 주별, 일별)
     @GetMapping("{liquidId}/trend")
+    @Operation(
+            summary = "특정 물통 트렌드 조회 (by liquidId)",
+            description = "liquidId로 특정 물통 트렌드(시간 구간대별 무게/누적합) 조회 (월별, 주별, 일별)"
+    )
     public ApiResponse<Object> readLiquidTrendByID(@PathVariable Long liquidId, @RequestParam(name = "period", defaultValue = "DAILY") PeriodType period,
                                                        @RequestParam(name = "date", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
                                                        @RequestParam(name = "mode", defaultValue = "TREND") TrendMode mode) {
